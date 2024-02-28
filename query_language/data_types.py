fix annoying bug where numerical aggregations were converted to objects

query_language/data_types.py
@@ -579,6 +579,7 @@ class Events(TimeSeriesQueryable):
                                     uniques[np.where(np.isnan(grouped_values), -1, grouped_values).astype(int)])
         
         assert len(grouped_values) == len(index)
+        print(grouped_values.dtype, pd.Series(grouped_values, name=self.name).convert_dtypes())
         return TimeSeries(index, pd.Series(grouped_values, name=self.name).convert_dtypes())
         
     def compress(self):
@@ -863,7 +864,7 @@ class Intervals(TimeSeriesQueryable):
                                     uniques[np.where(np.isnan(grouped_values), -1, grouped_values).astype(int)])
         
         assert len(grouped_values) == len(index)
-        return TimeSeries(index, pd.Series(grouped_values, name=self.name).replace(np.nan, pd.NA))
+        return TimeSeries(index, pd.Series(grouped_values, name=self.name).convert_dtypes())
     
     def with_values(self, new_values, preserve_nans=False):
         return Intervals(self.df.assign(**{self.value_field: self.preserve_nans(new_values) if preserve_nans else new_values}),
@@ -1350,7 +1351,7 @@ class TimeSeries(TimeSeriesQueryable):
                                      duration)
         if uniques is not None:
             result = np.where(np.isnan(result), np.nan, uniques[np.where(np.isnan(result), -1, result).astype(int)])
-        return self.with_values(pd.Series(result, index=self.series.index, name=self.series.name).replace(np.nan, pd.NA))
+        return self.with_values(pd.Series(result, index=self.series.index, name=self.series.name).astype(self.series.dtype))
 
     def aggregate(self, start_times, end_times, agg_func):
         """

query_language/evaluator.py
@@ -277,8 +277,7 @@ class EvaluateExpression(lark.visitors.Transformer):
             if isinstance(expr, Events):
                 return expr.bin_aggregate(self.time_index, *time_bounds, agg_method[0])
             elif isinstance(expr, (Intervals, Compilable)):
-                result = expr.bin_aggregate(self.time_index, *time_bounds, agg_method[1], agg_method[0])
-                return result
+                return expr.bin_aggregate(self.time_index, *time_bounds, agg_method[1], agg_method[0])
             else:
                 raise ValueError(f"Only Events and Intervals can be bin-aggregated")
         else:
@@ -369,7 +368,7 @@ class EvaluateExpression(lark.visitors.Transformer):
         if args[1].value in ("mean", "median"):
             impute_method = args[1].value.lower()
             numpy_func = {"mean": np.nanmean, "median": np.nanmedian}[impute_method]
-            return var_exp.where(nan_mask, numpy_func(var_exp.get_values().replace(pd.NA, np.nan).astype(float)))
+            return var_exp.astype(np.float64).where(nan_mask, numpy_func(var_exp.get_values().replace(pd.NA, np.nan).astype(float)))
         else:
             impute_method = self._parse_literal(args[1].value)
             scalar = var_exp.get_values().dtype.type(impute_method)
@@ -558,7 +557,9 @@ class EvaluateQuery(lark.visitors.Interpreter):
                 var_exp = self.variable_transform(var_exp)
                 # var_exp = var_exp.with_values(pd.Series(sf.discretization.discretize_column(var_exp.name, var_exp.get_values(), self.discretization_spec)[0], 
                 #                                         name=var_exp.name))
+            print(var_exp.name, "before:", var_exp.get_values().dtype, var_exp)
             var_exp = var_exp.compress()
+            print(var_exp.name, "after:", var_exp.get_values().dtype)
             
             if self.cache is not None:
                 self.cache.save((tree_desc, options_desc), var_exp, transform_info=self.variable_transform_desc, time_index_tree=time_index_tree)